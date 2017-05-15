5301adc5fbce60dddc006e3b98a8f2f582b000d1
NoahSekreter <nsekreter@gmail.com>
2017-05-15 09:08:39 -0400

    Added JavaScript magic

---
+++ Wardrobe/Scripts/Script.js
@@ -0,0 +1,10 @@
+﻿$(document).mousemove(function (e) {
+    var $width = ($(document).width()) / 255;
+    var $height = ($(document).height()) / 255;
+    var $pageX = parseInt(e.pageX / $width, 10);
+    var $pageY = parseInt(e.pageY / $height, 10);
+
+    $("hr").css("border-top", "2px solid rgb(" + 200 + "," + $pageX + "," + $pageY + ")");
+    $(".thumbnail").css("border", "3px solid rgb(" + $pageX + "," + 200 + "," + $pageY + ")");
+    $(".btn").css("border", "3px solid rgb(" + 200 + "," + $pageX + "," + $pageY + ")");
+});
